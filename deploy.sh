080678bc5e23147a0a846cf8589a487850320118
NamitaMe <88753484+NamitaMe@users.noreply.github.com>
2022-01-05 15:24:40 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo "Starting to deploy docker image.."
-DOCKER_IMAGE=turkogluc/spring-jenkins-demo
+DOCKER_IMAGE=namiducker/java-test:2.0.0
 docker pull $DOCKER_IMAGE
 docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
 docker run -d -p 8080:8080 $DOCKER_IMAGE
