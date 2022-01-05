e419b23ee07547c179e2c339ce246684b8e864b5
NamitaMe <88753484+NamitaMe@users.noreply.github.com>
2022-01-05 15:40:26 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ echo "Starting to deploy docker image.."
 DOCKER_IMAGE=namiducker/java-test:2.0.0
 docker pull $DOCKER_IMAGE
 docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
-docker run -d -p 8080:8080 $DOCKER_IMAGE
+docker run -d -p 8081:8081 $DOCKER_IMAGE
